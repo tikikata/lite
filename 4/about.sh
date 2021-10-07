26233ad42c0dcf7ad26d7c7fe714552c609bebf9
tikikata <86051875+tikikata@users.noreply.github.com>
2021-10-07 13:17:13 +0800

    Add files via upload

---
+++ 4/about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "==================================================="
+echo -e "#            Autoscript By RexNotRed              #"
+echo -e "#-------------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit                 #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit          #"
+echo -e "# For VPS with KVM and VMWare virtualization      #"
+echo -e "# Autoscript By RexNotRed                         #"
+echo -e "#-------------------------------------------------#"
+echo -e "#        THANKS FOR PURCHASE THIS SCRIPTS         #"
+echo -e "#-------------------------------------------------#"
+echo -e "#                                                 #"
+echo -e "# Thanks to : Horasss & Wildy-Project             #"
+echo -e "#                                                 #"
+echo -e "# Telegram : t.me/rexnotred                       #"
+echo -e "#                                                 #"
+echo -e "#-------------------------------------------------#"
+echo -e "#         © Copyright 2021 by RexNotRed           #"
+echo -e "==================================================="
